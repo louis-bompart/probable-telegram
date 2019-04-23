777e7ec06ae0a66eef579591a5d9cfeb99fad8c1
Louis Bompart <lbompart@coveo.com>
2019-04-23 16:02:39 -0400

    foo

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const { imgWatch } = require('./tasks/img.tasks');
 const { styleWatch } = require('./tasks/style.tasks');
 
 const watch = gulp.parallel(styleWatch, imgWatch);
-const uploadPackageRelease = require('./tasks/uploadPackage.tasks').uploadPackageRelease;
+const uploadPackageRelease = require('./tasks/uploadPackage.tasks.js').uploadPackageRelease;
 
 const dev = gulp.parallel(watch, () => {
   return gulp.watch('somefile');
